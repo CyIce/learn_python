75e88ace2631cc465f01851eea8dd9743f24e4b8
cyice <1207201395@qq.com>
2018-07-13 00:58:40 +0800

    Initial commit

---
+++ 自动化处理/音乐播放/play_music.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/13 00:08
+# @Author  : CyIce
+# @Email   : 1207201395@qq.com
+# @File    : play_music.py
+# @Software: PyCharm
+
+import pygame
+import time
+
+# 音乐路径
+file_path="./res/云烟成雨.flac"
+
+# 初始化
+pygame.mixer.init()
+
+# 加载音乐
+track=pygame.mixer.music.load(file_path)
+
+# 播放
+pygame.mixer.music.play()
+# 持续播放
+time.sleep(10)
+# 暂停
+pygame.mixer.music.pause()
+
+pygame.mixer.music.play()
+# 停止
+pygame.mixer.stop()
